473002562d4dde38664480012fd1690523a60f00
dekc89 <dekc89@gmail.com>
2018-11-16 17:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function theBeatlesPlay(arrMus, arrArt) {
   var empty = [];
   for (let i = 0; i < arrMus.length; i++) {
-    empty = arrMus[i] + " plays " + arrArt[i];
+    empty.push(arrMus[i] + " plays " + arrArt[i]);
   }
   return empty;
+}
+
+
+function johnLennonFacts(array) {
+  
 }
